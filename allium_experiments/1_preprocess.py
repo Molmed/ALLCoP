9fa3ab333626befad14e277f44648b2c2396f5a4
Mariya Lysenkova Wiklander <mariya.lysenkova@medsci.uu.se>
2024-10-16 17:40:37 +0200

    Refactored formatting logic

--- allium_experiments/1_preprocess.py
+++ allium_experiments/1_preprocess.py
@@ -22,7 +22,24 @@ for data_file in raw_data_files:
 # Merge the dataframes
 merged_df = pd.concat(dataframes)
 
-print(merged_df)
+# Report all items where the known_class is empty
+print('Items with empty known_class:')
+print(merged_df[merged_df['known_class'].isnull()])
+
+# Drop all rows where known_class is empty
+merged_df = merged_df.dropna(subset=['known_class'])
+
+# Report all items where known_class is "B-other"
+print('Items with known_class as B-other:')
+b_others = merged_df[merged_df['known_class'] == 'B-other']
+print(b_others)
+
+# Save the b_others
+b_others.to_csv(os.path.join(data_dir, 'formatted_predictions_b_other.csv'),
+                index=False)
+
+# Remove b-others from merged_df
+merged_df = merged_df[merged_df['known_class'] != 'B-other']
 
 # Dump to csv
 merged_df.to_csv(
