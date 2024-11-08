3fc9e3e7988c3fdb0f724aa65ccc9a46ee3982b7
Mariya Lysenkova Wiklander <mariya.lysenkova@medsci.uu.se>
2024-11-08 10:06:19 +0100

    Process b-other cases across different error rates

--- allium_experiments/4_unknowns.py
+++ allium_experiments/4_unknowns.py
@@ -4,8 +4,6 @@ from conformist import FNRCoP, PredictionDataset, \
 from lib.constants import FORMATTED_PREDICTIONS_FILE, \
     FORMATTED_PREDICTIONS_FILE_B_OTHER, OUTPUT_DIR
 
-OUTPUT_DIR_B_OTHER = f'{OUTPUT_DIR}/b_other'
-
 # Here, we use all known data for calibration and then get prediction
 # sets for all of our validation cohorts
 # Read in formatted predictions
@@ -15,9 +13,22 @@ cal_pd = PredictionDataset(predictions_csv=FORMATTED_PREDICTIONS_FILE,
 val_pd = PredictionDataset(predictions_csv=FORMATTED_PREDICTIONS_FILE_B_OTHER,
                            dataset_col_name='dataset')
 
-# CALIBRATE CONFORMAL PREDICTOR #
-mcp = FNRCoP(cal_pd, alpha=0.10)
-mcp.calibrate()
+print(f'Calibration dataset size: {cal_pd.df.shape[0]}')
+print(f'Prediction dataset size: {val_pd.df.shape[0]}')
 
-# PREDICT #
-formatted_predictions = mcp.predict(val_pd, OUTPUT_DIR_B_OTHER)
+alphas = [0.075, 0.1, 0.15]
+colors = ['#4f759b', '#5d5179', '#571f4e']
+
+for alpha in alphas:
+    # CALIBRATE CONFORMAL PREDICTOR #
+    mcp = FNRCoP(cal_pd, alpha=alpha)
+    mcp.calibrate()
+
+    alpha_str = str(alpha).replace('.', '')
+    output_dir = f'{OUTPUT_DIR}/b_other_{alpha_str}'
+
+    # PREDICT #
+    color = colors.pop(0)
+    formatted_predictions = mcp.predict(val_pd,
+                                        output_dir,
+                                        upset_plot_color=color)
