f94fca36cf20c44097386d13d807fd97deba6b0d
Mariya Lysenkova Wiklander <mariya.lysenkova@medsci.uu.se>
2024-11-21 16:04:01 +0100

    Split up reports generation

--- allium_experiments/2_validate.py
+++ allium_experiments/2_validate.py
@@ -1,24 +1,34 @@
 from conformist import AlphaSelector, \
     FNRCoP, PredictionDataset, ModelVsCopFNR
 from lib.dual_subtype_heatmap import DualSubtypeHeatmap
-from lib.constants import FORMATTED_PREDICTIONS_FILE, \
-    FORMATTED_PREDICTIONS_FILE_ALL, \
+from lib.constants import FORMATTED_PREDICTIONS_FILE_ALL, \
+    FORMATTED_PREDICTIONS_FILE_SINGLECLASS, \
     OUTPUT_DIR, \
     COLOR_PALETTE
 
 
-OUTPUT_DIR_DATASET = f'{OUTPUT_DIR}/dataset'
-
 # Create reports for entire dataset
-apd = PredictionDataset(predictions_csv=FORMATTED_PREDICTIONS_FILE_ALL,
-                        dataset_col_name='dataset')
+apd_all = PredictionDataset(predictions_csv=FORMATTED_PREDICTIONS_FILE_ALL,
+                            dataset_col_name='dataset')
 
-# Get class counts and prediction heatmap
-apd.run_reports(OUTPUT_DIR_DATASET,
-                upset_plot_color='#457b9d',
-                min_softmax_threshold=0.5,
-                primary_class_only_in_class_counts=True,
-                custom_color_palette=COLOR_PALETTE)
+apd_all.create_reports_dir(f'{OUTPUT_DIR}/dataset_all')
+
+apd_all.visualize_class_counts_by_dataset(
+    primary_class_only=True,
+    custom_color_palette=COLOR_PALETTE)
+
+apd_all.visualize_prediction_stripplot(
+    custom_color_palette=COLOR_PALETTE)
+
+# Now create reports for single known subtype dataset
+apd_filtered = PredictionDataset(
+    predictions_csv=FORMATTED_PREDICTIONS_FILE_SINGLECLASS,
+    dataset_col_name='dataset')
+
+apd_filtered.create_reports_dir(f'{OUTPUT_DIR}/dataset_singleclass')
+apd_filtered.softmax_summary()
+apd_filtered.visualize_prediction_heatmap()
+apd_filtered.visualize_model_sets(0.5, '#457b9d')
 
 exit()
 
