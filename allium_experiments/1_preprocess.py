9b623a2bf58cbe74464cfe6a391e8fba3befc03b
Mariya Lysenkova Wiklander <mariya.lysenkova@medsci.uu.se>
2024-11-21 10:17:23 +0100

    Update formatted predictions filtering and add custom color palette

--- allium_experiments/1_preprocess.py
+++ allium_experiments/1_preprocess.py
@@ -4,6 +4,7 @@ from lib.allium_data_preprocessor import AlliumDataPreprocessor
 from lib.constants import RAW_PREDICTIONS_DIR, \
     FORMATTED_PREDICTIONS_FILE, \
     FORMATTED_PREDICTIONS_FILE_B_OTHER, \
+    FORMATTED_PREDICTIONS_FILE_MULTICLASS, \
     FORMATTED_PREDICTIONS_FILE_ALL, \
     DATASET_METADATA
 
@@ -32,8 +33,22 @@ print(merged_df[merged_df['known_class'].isnull()])
 # Drop all rows where known_class is empty
 merged_df = merged_df.dropna(subset=['known_class'])
 
-# Dump to csv
+# Dump all items except empty known class to csv
+print('All items with recognized subtype or B-other:')
+print(merged_df)
 merged_df.to_csv(FORMATTED_PREDICTIONS_FILE_ALL, index=False)
+
+# Report all the multiclass items, this means known_class will contain comma
+print('Items with multiple known subtypes:')
+multiclass_items = merged_df[merged_df['known_class'].str.contains(',')]
+print(multiclass_items)
+
+# Save the multiclass items
+multiclass_items.to_csv(FORMATTED_PREDICTIONS_FILE_MULTICLASS,
+                        index=False)
+
+# Remove multiclass items from merged_df
+merged_df = merged_df[~merged_df['known_class'].str.contains(',')]
 
 # Report all items where known_class is "B-other"
 print('Items with known_class as B-other:')
@@ -47,5 +62,7 @@ b_others.to_csv(FORMATTED_PREDICTIONS_FILE_B_OTHER,
 # Remove b-others from merged_df
 merged_df = merged_df[merged_df['known_class'] != 'B-other']
 
-# Dump to csv
+# Dump remaining to csv
+print('Items with single known subtype:')
+print(merged_df)
 merged_df.to_csv(FORMATTED_PREDICTIONS_FILE, index=False)
--- allium_experiments/lib/constants.py
+++ allium_experiments/lib/constants.py
@@ -13,10 +13,32 @@ with open(f'{script_dir}/datasets.yml', 'r') as file:
     DATASET_METADATA = dm['datasets']
 
 FORMATTED_PREDICTIONS_FILE = \
-    os.path.join(DATA_DIR, 'formatted_predictions_known_subtype.csv')
+    os.path.join(DATA_DIR, 'formatted_predictions_single_known_subtype.csv')
 FORMATTED_PREDICTIONS_FILE_B_OTHER = \
     os.path.join(DATA_DIR, 'formatted_predictions_b_other.csv')
+FORMATTED_PREDICTIONS_FILE_MULTICLASS = \
+    os.path.join(DATA_DIR, 'formatted_predictions_dual_known_subtype.csv')
 FORMATTED_PREDICTIONS_FILE_ALL = \
-    os.path.join(DATA_DIR, 'formatted_predictions_known_and_b_other.csv')
+    os.path.join(DATA_DIR, 'formatted_predictions.csv')
 
 OUTPUT_DIR = os.path.join(script_dir, '../../output')
+
+COLOR_PALETTE = {'high hyperdiploid': '#0F4D92',
+                 'low hyperdiploid': '#1f77b4',
+                 'iAMP21': '#17becf',
+                 'hypodiploid': '#ACE5EE',
+                 'ETV6::RUNX1': '#2ca02c',
+                 'ETV6::RUNX1-like': '#98df8a',
+                 'T-ALL': '#CD5C5C',
+                 'KMT2A-r': '#FF7F50',
+                 'NUTM1-r': '#ffbb78',
+                 'PAX5alt': '#FC8EAC',
+                 'PAX5 P80R': '#FBCCE7',
+                 'TCF3::PBX1': '#20B2AA',
+                 'MEF2D-r': '#30D5C8',
+                 'BCR::ABL1': '#9467bd',
+                 'BCR::ABL1-like': '#c5b0d5',
+                 'DUX4-r': '#004953',
+                 'ZNF384-r': '#8A496B',
+                 'B-other': '#7f7f7f',
+                 'multiclass': '#bcbd22'}
